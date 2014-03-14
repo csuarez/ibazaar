a77d889a788652118e4e90de8e81edf6bc034688
csuarez <suarez.ortega.cesar@gmail.com>
2014-03-14 17:52:47 +0100

    First version for app profiles.

---
+++ src/IBazaar/FrontendBundle/Controller/AppController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace IBazaar\FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class AppController extends Controller
+{
+    /**
+     * @Route("/app/{id}")
+     * @Template()
+     */
+    public function profileAction($id) {
+    	$app = $this->getDoctrine()
+					->getRepository('IBazaarDataModelBundle:App')
+					->findOneById($id);
+
+    	if (!$app) {
+    		throw $this->createNotFoundException('This app does not exist.');
+    	}
+
+        return array(
+        	'app' => $app
+        );
+    }
+}
+?>
